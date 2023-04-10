25dfb32a7a9d9ba4211a0199388001eb947ca900
bluehyssopu <1767807919@qq.com>
2023-04-10 23:12:14 +0800

    实现座位预约功能

--- router_handler/seatinfo.js
+++ router_handler/seatinfo.js
@@ -29,6 +29,7 @@ exports.getSeatInfoList = (req, res) => {
 }
 
 exports.getSeatZoneList = (req, res) => {
+    // console.log(req.body);
     const sql = `select seat_area, seat_number, seat_type, status from seat where seat_area=?`
 
     db.query(sql, req.body.seat_area, (err, results) => {
@@ -44,5 +45,54 @@ exports.getSeatZoneList = (req, res) => {
             message: '获取该区域座位信息成功！',
             data: results,
         })
+    })
+}
+
+exports.reserveSeat = (req, res) => {
+    // console.log(req.body);
+    const user_id = req.body.user_id
+    const seat_id = req.body.seat_id
+    const startTime = req.body.startTime; // 前端发送的开始时间，格式为 HH:MM
+    const endTime = req.body.endTime; // 前端发送的结束时间，格式为 HH:MM
+    const date = new Date().toLocaleDateString()
+    // 将开始时间和结束时间转换为 MySQL 格式的日期时间字符串
+    const startDateTime = startTime + ':00';
+    const endDateTime = endTime + ':00';
+  
+    // 查询数据库，判断该时间段内是否已经有预约
+    const query = 'SELECT * FROM reservation WHERE seat_id = ? AND start_time < ? AND end_time > ?';
+    db.query(query, [seat_id, endDateTime, startDateTime], (error, results, fields) => {
+        if (error) {
+            console.log(error);
+            return res.cc("查询出错")
+        } else if (results.length > 0) {
+            console.log(results);
+            return res.cc("该段时间内已有预约。This time slot has been booked")
+        } else { // 时间段内没有预约，创建新的预约记录
+            const insertQuery = 'INSERT INTO reservation (user_id, seat_id, date, start_time, end_time) VALUES   (?, ?, ?, ?, ?)  '
+            db.query(insertQuery, [user_id, seat_id, date, startDateTime, endDateTime], (error, results, fields) => {
+                if (error) {
+                    console.log(error);
+                    return res.cc("创建预约记录出错")
+                } else { 
+                    return res.cc("创建预约记录成功，返回成功信息")
+                }
+            })
+        }
+    })
+}
+
+exports.cancelSeat = (req, res) => {
+    const id = req.reserveId // 预约记录的 ID
+    // 删除数据库中相应的预约记录
+    const query = 'DELETE FROM reservation WHERE id = ?'
+    db.query(query, [id], (error, results, fields) => { 
+        if (error) { // 删除出错，返回错误信息 
+            res.status(500).send('Server error'); 
+        } else if (results.affectedRows === 0) { // 没有找到相应的预约记录，返回错误信息 
+            res.status(404).send('Appointment not found')
+        } else { // 删除成功，返回成功信息 
+            res.status(200).send('Cancelled successfully')
+        } 
     })
 }
