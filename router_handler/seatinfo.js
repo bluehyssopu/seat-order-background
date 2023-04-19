46ad5fc61cb93add99b587ba04e3c07caf61a34b
bluehyssopu <1767807919@qq.com>
2023-04-19 21:46:31 +0800

    补充座位状态更新函数逻辑

--- router_handler/seatinfo.js
+++ router_handler/seatinfo.js
@@ -7,6 +7,7 @@ function updateSeatStatus(id) {
     const date = new Date().toLocaleDateString()
     const nowTime = new Date().getTime() // yy:mm:dd hh:mm:ss 利用getTime()比较时间戳
     const sql = 'SELECT * FROM reservation WHERE seat_id = ? AND date = ?';
+    let boolStatus = false
     db.query(sql, [seat_id, date], (err, results) => {
         if (err) {
             return err
@@ -17,18 +18,32 @@ function updateSeatStatus(id) {
                 console.log(startTime + " " + nowTime + " " + endTime);
                 if (nowTime >= startTime && nowTime < endTime) {
                     // 座位表中 座位号为 seat_number 不是 seat_id
+                    boolStatus = true
                     const statusSql = "update seat set status = '使用中' WHERE seat_number = ?"
                     db.query(statusSql, seat_id, (results) => {
                         if (err) {
                             return err
                         } else {
+                            console.log("座位状态更新成功");
                             return "座位状态更新成功"
                         }
                     })
                 }
             }
         }
-
+        if (boolStatus === false) {
+            const statusFreeSql = "update seat set status = '空闲' WHERE seat_number = ?"
+            db.query(statusFreeSql, seat_id, (err2, results) => {
+                if (err2) {
+                    return err2
+                } else {
+                    console.log("座位状态更新为空闲");
+                    return "座位状态更新为空闲"
+                }
+            })
+        } else {
+            return
+        }
     })
 }
 
