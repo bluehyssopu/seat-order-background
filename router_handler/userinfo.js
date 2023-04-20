ffa7cfa9c722450484755af56c4c82700369157c
bluehyssopu <1767807919@qq.com>
2023-04-20 22:47:26 +0800

    增加用户信息的诚信值返回

--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -11,18 +11,27 @@ exports.getUserInfo = (req, res) => {
 
     // 注意：req 对象上的 user 属性，是 Token 解析成功，express-jwt 中间件帮我们挂载上去的
     db.query(sql, req.user.id, (err, results) => {
+        const user_id = req.user.id
         // 1. 执行 SQL 语句失败
         if (err) return res.cc(err)
         
         // 2. 执行 SQL 语句成功，但是查询到的数据条数不等于 1
-        if (results.length !== 1) return res.cc('获取用户信息失败！')
+        else if (results.length !== 1) return res.cc('获取用户信息失败！')
         
+        else {
+            const creditSql = 'select credit_value from credit where user_id=?'
+            db.query(creditSql, user_id, (err2, results2) => {
+                if (err2) return err2
+                const newResult = { ...results[0], ...results2[0] }
+                res.send({
+                    status: 0,
+                    message: '获取用户基本信息成功！',
+                    data: newResult,
+                })
+            })
+        }
         // 3. 将用户信息响应给客户端
-        res.send({
-            status: 0,
-            message: '获取用户基本信息成功！',
-            data: results[0],
-        })
+        
     })
   
 }
